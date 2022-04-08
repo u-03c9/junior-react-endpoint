107729db2f073fd419a263ea6ac10d2df85496ce
Peter Khalil <uni.03c9@gmail.com>
2022-04-08 15:00:51 +0200

    enable cros

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from "apollo-server";
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+  cors: true,
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`);
 });
